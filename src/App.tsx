5e973918d4584f54c3bf1642b93b469bf4090c47
jieun <jieunh0311@gmail.com>
2025-04-11 10:38:52 +0900

    Board List 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import { appContainer, board, buttons } from './App.css'
-
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { appContainer, board, buttons } from "./App.css";
+import BoardList from "./components/BoardList/BoardList";
 
 function App() {
+  const [activeBoardId, setActivBoardId] = useState("board-0");
+
   return (
     <div className={appContainer}>
-      <div className = {board}>
-
+      <div className={board}>
+        <BoardList
+          activeBoardId={activeBoardId}
+          setActiveBoardId={setActivBoardId}
+        ></BoardList>
       </div>
 
-      <div className= {buttons}>
-        <button>
-          이 게시판 삭제하기
-        </button>
-        <button>
-
-        </button>
+      <div className={buttons}>
+        <button>이 게시판 삭제하기</button>
+        <button></button>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
