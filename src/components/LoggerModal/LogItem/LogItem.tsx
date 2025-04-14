51d0eee11465316de89066ce22978610e5e1a67d
jieun <jieunh0311@gmail.com>
2025-04-14 15:38:06 +0900

    LogItem 스타일 생성

--- src/components/LoggerModal/LogItem/LogItem.tsx
+++ src/components/LoggerModal/LogItem/LogItem.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 import { ILogItem } from '../../../types'
 import { BsFillPersonFill } from 'react-icons/bs';
+import { author, date, logItemWrap, message } from './LogItem.css';
 
 type TLogItemProps = {
   logItem : ILogItem;
@@ -16,13 +17,13 @@ const LogItem: FC<TLogItemProps> = ({logItem}) => {
     ${timeOffset.getSeconds() === 0 ? `just now` : ""}`
 
   return (
-    <div>
-      <div>
+    <div className={logItemWrap}>
+      <div className={author}>
         <BsFillPersonFill />
         {logItem.logAuthor}
       </div>
-      <div>{logItem.logMessage}</div>
-      <div>{showOffsetTime}</div>
+      <div className={message}>{logItem.logMessage}</div>
+      <div className={date}>{showOffsetTime}</div>
     </div>
   )
 }
