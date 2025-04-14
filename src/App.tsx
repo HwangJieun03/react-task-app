dafc3e34a733376d40c26576330a958ddf9bd5e3
jieun <jieunh0311@gmail.com>
2025-04-14 15:53:31 +0900

    게시판 삭제 기능 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import { useState } from "react";
 //import reactLogo from "./assets/react.svg";
-import { appContainer, board, buttons, delteBorderButton, loggerButton } from "./App.css";
+import {
+  appContainer,
+  board,
+  buttons,
+  delteBorderButton,
+  loggerButton,
+} from "./App.css";
 import BoardList from "./components/BoardList/BoardList";
 import ListsContainer from "./components/ListsContainer/ListsContainer";
-import { useTypedSelector } from "./hooks/redux";
+import { useTypedDispatch, useTypedSelector } from "./hooks/redux";
 import EditModal from "./components/EditModal/EditModal";
 import LoggerModal from "./components/LoggerModal/LoggerModal";
+import { deleteBoard } from "./store/slices/boardsSlice";
+import { v4 } from "uuid";
+import { addLog } from "./store/slices/loggerSlice";
 
 function App() {
+  const dispatch = useTypedDispatch();
+
   const [isLoggerOpen, setIsLoggerOpen] = useState(false);
   const [activeBoardId, setActivBoardId] = useState("board-0");
 
@@ -21,9 +32,35 @@ function App() {
 
   const lists = getActiveBoard.lists;
 
+  const handleDeleteBoard = () => {
+    if (boards.length > 1) {
+      dispatch(deleteBoard({ boardId: getActiveBoard.boardId }));
+      dispatch(
+        addLog({
+          logId: v4(),
+          logMessage: `게시판 지우기: ${getActiveBoard.boardName}`,
+          logAuthor: "User",
+          logTimestamp: String(Date.now()),
+        })
+      );
+
+      const newIndexToSet = () => {
+        const indexToBeDeleted = boards.findIndex(
+          (board) => board.boardId === activeBoardId
+        );
+        return indexToBeDeleted === 0
+          ? indexToBeDeleted + 1
+          : indexToBeDeleted - 1;
+      };
+      setActivBoardId(boards[newIndexToSet()].boardId);
+    } else {
+      alert("최소 게시판 개수는 한 개입니다.");
+    }
+  };
+
   return (
     <div className={appContainer}>
-      {isLoggerOpen ? <LoggerModal setIsLoggerOpen={setIsLoggerOpen}/> : null}
+      {isLoggerOpen ? <LoggerModal setIsLoggerOpen={setIsLoggerOpen} /> : null}
       {modalActive ? <EditModal /> : null}
       <BoardList
         activeBoardId={activeBoardId}
@@ -32,8 +69,15 @@ function App() {
       <div className={board}></div>
       <ListsContainer lists={lists} boardId={getActiveBoard.boardId} />
       <div className={buttons}>
-        <button className={delteBorderButton}>이 게시판 삭제하기</button>
-        <button className={loggerButton} onClick={()=>setIsLoggerOpen(!isLoggerOpen)}>{isLoggerOpen?"활동 목록 숨기기": "활동 목록 보이기"}</button>
+        <button className={delteBorderButton} onClick={handleDeleteBoard}>
+          이 게시판 삭제하기
+        </button>
+        <button
+          className={loggerButton}
+          onClick={() => setIsLoggerOpen(!isLoggerOpen)}
+        >
+          {isLoggerOpen ? "활동 목록 숨기기" : "활동 목록 보이기"}
+        </button>
       </div>
     </div>
   );
--- src/store/slices/boardsSlice.ts
+++ src/store/slices/boardsSlice.ts
@@ -32,6 +32,10 @@ type TDeleteTaskAction = {
   taskId: string;
 };
 
+type TDeleteBoardAction = {
+  boardId: string;
+};
+
 const initialState: TBoardsState = {
   modalActive: false,
   boardArray: [
@@ -80,6 +84,12 @@ const boardsSlice = createSlice({
   reducers: {
     addBoard: (state, { payload }: PayloadAction<TAddBoardAction>) => {
       state.boardArray.push(payload.board);
+    },
+
+    deleteBoard: (state, { payload }: PayloadAction<TDeleteBoardAction>) => {
+      state.boardArray = state.boardArray.filter(
+        (board) => board.boardId !== payload.boardId
+      );
     },
 
     addList: (state, { payload }: PayloadAction<TAddListAction>) => {
@@ -166,7 +176,15 @@ const boardsSlice = createSlice({
   },
 });
 
-export const { addBoard, deleteList, setModalActive, addList, addTask, updateTask, deleteTask } =
-  boardsSlice.actions;
+export const {
+  addBoard,
+  deleteBoard,
+  deleteList,
+  setModalActive,
+  addList,
+  addTask,
+  updateTask,
+  deleteTask,
+} = boardsSlice.actions;
 
 export const boardsReducer = boardsSlice.reducer;
