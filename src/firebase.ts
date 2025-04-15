46f5ae99e7b6c750b33a8f7e67511f87b9ed18b9
jieun <jieunh0311@gmail.com>
2025-04-15 14:07:40 +0900

    Firebase 연결

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA1CvG0AE3eeuGZiPArVh7Vipqj3djaArY",
+  authDomain: "react-task-app-37918.firebaseapp.com",
+  projectId: "react-task-app-37918",
+  storageBucket: "react-task-app-37918.firebasestorage.app",
+  messagingSenderId: "370856473903",
+  appId: "1:370856473903:web:7eaa143da8800f36f81dda"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
